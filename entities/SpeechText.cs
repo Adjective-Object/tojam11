ff76ba9f381f096b67e0a48d2ac9a48827de27cc
Max <mhuan13@gmail.com>
2016-05-06 22:54:40 -0400

    Selecting from a list of options

--- entities/SpeechText.cs
+++ entities/SpeechText.cs
@@ -23,7 +23,16 @@ namespace Adventure
 		public static SpeechText Spawn(String fontName, Vector2 position, String text, SpeechMode mode = SpeechMode.AMBIENT) {
 			SpeechText e = new SpeechText (fonts [fontName], position, text, mode);
 			AdventureGame.SpawnEntity(e);
+
+			e.position -= LETTER_OFFSET * text.Length / 2;
+
 			return e;
+		}
+		public static SpeechText Spawn(String fontName, Vector2 position, String text, Option[] options, Func<Boolean> walkAwayAction) {
+			SpeechText e = new SpeechText (fonts [fontName], position, text, options, walkAwayAction);
+			AdventureGame.SpawnEntity(e);
+			e.position -= LETTER_OFFSET * text.Length / 2;
+			return e;	
 		}
 
 		//
@@ -31,16 +40,21 @@ namespace Adventure
 		//
 		static Vector2 STARTING_OFFSET = new Vector2(0,10);
 		static Vector2 LETTER_OFFSET = new Vector2(15,0);
+		static Vector2 LINE_HEIGHT = new Vector2(0,25);
 		static Vector2 GRADUAL_RISE = new Vector2(0,-10);
 		static double ANIMATION_TIME = 0.4;
 		static double LETTER_SPAWN_DELAY = 0.03;
 		static double AMBIENT_SPEECH_LIFETIME = 3.0;
 
+
 		override public Boolean isUI {
 			get { return true; }
 		}
-		public Boolean doneEmitting {
+		public Boolean DoneEmitting {
 			get { return this.ages[this.ages.Length - 1] > ANIMATION_TIME; }
+		}
+		public Boolean IsDismissed {
+			get { return this.dismissedTime >= 0; }
 		}
 
 		SpriteFont font;
@@ -53,9 +67,31 @@ namespace Adventure
 			this.text = text;
 			this.font = font;
 			this.mode = mode;
-			this.ages = new double[text.Length];
 			this.dismissedTime = -1;
-			for (int i = 0; i < text.Length; i++) {
+
+			if (mode != SpeechMode.PLAYER_ANSWER_QUESTION) {
+				this.ages = new double[text.Length];
+				for (int i = 0; i < text.Length; i++) {
+					this.ages [i] = i * -LETTER_SPAWN_DELAY;
+				}
+			}
+		}
+
+		Option[] options = null;
+		Func<Boolean> checkWalkAway;
+		int selectionIndex = 0;
+		protected SpeechText(SpriteFont font, Vector2 position, String text, Option[] options, Func<Boolean> checkWalkAway)
+			: this(font, position, text, SpeechMode.PLAYER_ANSWER_QUESTION){
+			this.options = options;
+			this.checkWalkAway = checkWalkAway;
+
+			int totalStringLength = text.Length;
+			foreach (Option o in options) {
+				totalStringLength += o.text.Length;
+			}
+
+			this.ages = new double[totalStringLength];
+			for (int i = 0; i < totalStringLength; i++) {
 				this.ages [i] = i * -LETTER_SPAWN_DELAY;
 			}
 		}
@@ -79,12 +115,43 @@ namespace Adventure
 					// if it's still animating in and the enter button is pressed, speed up the animation
 					if (ages [ages.Length - 1] < ANIMATION_TIME) {
 						this.timeScale = 3.0;
-					} else if (dismissedTime < 0){
+					} else if (this.IsDismissed){
 						this.dismissedTime = ages[ages.Length - 1];
 					}
 				}
 				break;
+			case SpeechMode.PLAYER_ANSWER_QUESTION:
+				if (! this.IsDismissed) {
+					if (Input.KeyPressed (Key.ENTER)) {
+						if (ages [ages.Length - 1] < ANIMATION_TIME) {
+							// if it's still animating in and the enter button is pressed, speed up the animation
+							this.timeScale = 3.0;
+						} else {
+							// otherwise, select the currently selected option
+							this.options [this.selectionIndex].callback ();
+							this.dismissedTime = this.ages [this.ages.Length - 1];
+						}
+
+						// TODO keys for up & down to change selection
+					}
+
+					// toggling between options
+					if (Input.KeyPressed (Key.TAB)) {
+						if (Input.KeyDown (Key.SHIFT)) {
+							this.selectionIndex = (selectionIndex + this.options.Length - 1) % this.options.Length;
+						} else {
+							this.selectionIndex = (selectionIndex + 1) % this.options.Length;
+						}
+					}
+
+					// check for walk away
+					if (checkWalkAway ()) {
+						this.dismissedTime = this.ages [this.ages.Length - 1];
+					}
+				}
+				break;
 			}
+			
 
 			if (dismissedTime > 0 && ages [ages.Length - 1] > dismissedTime + ANIMATION_TIME) {
 				this._alive = false;	
@@ -92,12 +159,33 @@ namespace Adventure
 		}
 
 		public override void Draw(SpriteBatch batch, GameTime time) {
-			for  (int i = 0; i < this.ages.Length; i++) {
+			if (mode != SpeechMode.PLAYER_ANSWER_QUESTION) {
+				DrawString (batch, this.position, this.text, this.ages);
+			} else {
+				DrawString (batch, this.position, this.text, this.ages);
+				int ageOffset = this.text.Length;
+				for (int i=0; i<this.options.Length; i++) {
+					DrawString (
+						batch, this.position + LINE_HEIGHT * (i + 1), this.options[i].text, 
+						this.ages, ageOffset,
+						i == this.selectionIndex ? new Color(255,255,0) : Color.White);
+					ageOffset += this.options[i].text.Length;
+				}
+			}
+		}
+
+		public void DrawString(SpriteBatch batch, Vector2 lineOrigin, String str, Double[] ages, 
+			int ageArrayOffset = 0, Color? textColorNullable = null) {
+			Color textColor = textColorNullable.GetValueOrDefault(Color.White);
+
+			for  (int i = 0; i < str.Length; i++) {
 				// calculate the animation time for this letter
-				double age = this.ages [i];
+				double age = ages [i + ageArrayOffset];
+				Console.WriteLine (ages + " " + age);
 
 				Vector2 offset;
 				float opacity;
+
 				// text is appearing still
 				if (dismissedTime < 0) {
 					float t = (float)Math.Max (0, Math.Min (1, age / ANIMATION_TIME));
@@ -116,8 +204,9 @@ namespace Adventure
 					opacity = (1 - t);
 				}
 
-				offset = offset - LETTER_OFFSET * text.Length / 2;
-				offset = offset + GRADUAL_RISE * (float) ages [0];
+				if (this.mode == SpeechMode.AMBIENT) {
+					offset = offset + GRADUAL_RISE * (float)ages [0];
+				}
 
 				// apply the calculated opacity and offset
 				foreach (Vector2 microOff in new Vector2[]{
@@ -125,24 +214,30 @@ namespace Adventure
 					new Vector2(1, 0), new Vector2(0, 1)}) {
 					batch.DrawString (
 						font, 
-						this.text [i].ToString (), 
-						this.position + offset + microOff,
+						str [i].ToString (), 
+						lineOrigin + offset + microOff,
 						Color.Black * (float) Math.Pow(opacity, 2));
 				}
 
 				batch.DrawString (
 					font, 
-					this.text [i].ToString (), 
-					this.position + offset,
-					Color.White * opacity);
-	
-				
-				
+					str [i].ToString (), 
+					lineOrigin + offset,
+					textColor * opacity);
 			}
 		}
 
 		public enum SpeechMode {
-			AMBIENT, PLAYER_CONTROLLED
+			AMBIENT, PLAYER_CONTROLLED, PLAYER_ANSWER_QUESTION
+		}
+
+		public class Option {
+			public String text;
+			public Action callback;
+			public Option(String text, Action callback) {
+				this.text = text;
+				this.callback = callback;
+			}
 		}
 
 	}
--- entities/behaviors/NPCCat.cs
+++ entities/behaviors/NPCCat.cs
@@ -5,13 +5,31 @@ namespace Adventure
 	public class NPCCat : CharacterBehavior
 	{
 		int interactionCount = 0;
-		override public void RespondToInteraction() {
+		string favorite;
+
+		override public void RespondToInteraction(Character player) {
+			// turn to face player
+			character.facingLeft = player.position.X < character.position.X;
+
+			// do conversation
 			switch (interactionCount) {
 			case 0:
-				EmitSpeech ("this party sucks..");
+				EmitSpeechOption ("what's your favorite?",
+					new SpeechText.Option[] {
+						new SpeechText.Option("chips", () => this.favorite = "chips"),
+						new SpeechText.Option("dip", () => this.favorite = "dip"),
+						new SpeechText.Option("salsa", () => this.favorite = "salsa"),
+					},
+					walkAway(
+						player, character,
+						() => {
+							this.favorite = "being an asshole"; 
+							EmitSpeech("where are you going?");
+						})
+				);
 				break;
 			case 1:
-				EmitSpeech ("so who dragged you here?");
+				EmitSpeech ("so you like " + favorite + "?");
 				break;
 			case 2:
 				EmitSpeech ("I'm tired.");
