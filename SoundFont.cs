dea6f545681ecce9953471b89cdb971c77e149a0
Max <mhuan13@gmail.com>
2016-05-07 11:54:39 -0400

    speech sounds

---
+++ SoundFont.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Adventure
+{
+	public class SoundFont
+	{
+		const int SOUNDFONT_COUNT = 110;
+		public SoundFont (String directoryName)
+		{
+		}
+
+		public void LoadContent(ContentManager content)
+		{
+		}
+	}
+}
+
